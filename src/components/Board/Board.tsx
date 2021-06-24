9a0119d12616a74348642b505a60857c8bde7663
aaronmivo <aamivo93@gmail.com>
2021-06-24 16:56:56 -0700

    Drag and drop boilerplate

--- src/components/Board/Board.tsx
+++ src/components/Board/Board.tsx
@@ -31,22 +31,41 @@ const backendColumns = {
     }
 }
 const onDragEnd = (result, columns, setColumns) => {
-    if(!result.destination) return;
-
-    const {source, destination} = result;
-    const column = columns[source.droppableId]
-    const copiedItems = [...column.items]
-    const [removed] = copiedItems.splice(source.index, 1)
-    copiedItems.splice(destination.index, 0, removed);
-    setColumns({
+    if (!result.destination) return;
+    const { source, destination } = result;
+  
+    if (source.droppableId !== destination.droppableId) {
+      const sourceColumn = columns[source.droppableId];
+      const destColumn = columns[destination.droppableId];
+      const sourceItems = [...sourceColumn.items];
+      const destItems = [...destColumn.items];
+      const [removed] = sourceItems.splice(source.index, 1);
+      destItems.splice(destination.index, 0, removed);
+      setColumns({
         ...columns,
         [source.droppableId]: {
-            ...column,
-            items: copiedItems
+          ...sourceColumn,
+          items: sourceItems
+        },
+        [destination.droppableId]: {
+          ...destColumn,
+          items: destItems
         }
-    })
-}
-
+      });
+    } else {
+      const column = columns[source.droppableId];
+      const copiedItems = [...column.items];
+      const [removed] = copiedItems.splice(source.index, 1);
+      copiedItems.splice(destination.index, 0, removed);
+      setColumns({
+        ...columns,
+        [source.droppableId]: {
+          ...column,
+          items: copiedItems
+        }
+      });
+    }
+  };
 function Board() {
     const [columns, setColumns] = useState(backendColumns);
     return (
@@ -57,6 +76,8 @@ function Board() {
             >
                 {Object.entries(columns).map(([id, column]) => {
                     return (
+                        <div>
+                        <h2>{column.name}</h2>
                         <Droppable droppableId={id} key={id}>
                             {(provided, snapshot) => {
                                 return (
@@ -102,6 +123,7 @@ function Board() {
                                 )
                             }}
                         </Droppable>
+                        </div>
                     )
                 })}
 
