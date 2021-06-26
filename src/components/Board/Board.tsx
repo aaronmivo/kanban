672a116f9e8b1036b1b3cc7f3cb3238b0275d7d0
aaronmivo <aamivo93@gmail.com>
2021-06-25 18:10:01 -0700

    Larger column and easier to land item

--- src/components/Board/Board.tsx
+++ src/components/Board/Board.tsx
@@ -7,8 +7,6 @@ import Column from '../Column/Column.tsx'
 
 
 const Board = () => {
-
-
     const initialColumns = {
         todo: {
           id: "todo",
@@ -40,63 +38,49 @@ const [columns, setColumns] = useState(initialColumns)
 
 
 const onDragEnd = ({ source, destination }) => {
-    // Make sure we have a valid destination
     if (destination === undefined || destination === null) return null;
 
-    // Make sure we're actually moving the item
     if (
       source.droppableId === destination.droppableId &&
       destination.index === source.index
     )
       return null;
 
-    // Set start and end variables
     const start = columns[source.droppableId];
     const end = columns[destination.droppableId];
 
-    // If start is the same as end, we're in the same column
     if (start === end) {
-      // Move the item within the list
-      // Start by making a new list without the dragged item
+
       console.log(start);
       const newList = start.list.filter((_, idx) => idx !== source.index);
 
-      // Then insert the item at the right location
       newList.splice(destination.index, 0, start.list[source.index]);
 
-      // Then create a new copy of the column object
       const newCol = {
         id: start.id,
         list: newList
       };
 
-      // Update the state
       setColumns((state) => ({ ...state, [newCol.id]: newCol }));
       return null;
     } else {
-      // If start is different from end, we need to update multiple columns
-      // Filter the start list like before
+
       const newStartList = start.list.filter((_, idx) => idx !== source.index);
 
-      // Create a new start column
       const newStartCol = {
         id: start.id,
         list: newStartList
       };
-
-      // Make a new end list array
       const newEndList = end.list;
 
-      // Insert the item into the end list
       newEndList.splice(destination.index, 0, start.list[source.index]);
 
-      // Create a new end column
+
       const newEndCol = {
         id: end.id,
         list: newEndList
       };
 
-      // Update the state
       setColumns((state) => ({
         ...state,
         [newStartCol.id]: newStartCol,
@@ -112,7 +96,11 @@ const onDragEnd = ({ source, destination }) => {
         {Object.values(columns).map((column) => {
           console.log(column)
           return (
-            <div>
+            <div style={{
+                display: "flex",
+                flexDirection: "column",
+                alignItems: "center"
+              }}>
               <Column column={column} key={column.id} />
             </div>
           )
--- src/components/Column/Column.tsx
+++ src/components/Column/Column.tsx
@@ -6,19 +6,19 @@ import ListItemCustom from '../ListItemCustom/ListItemCustom'
 const Column = ({ column }: any) => {
   return (
     <div
-      style={{
-        backgroundColor: 'gray',
-        margin: 10,
-        padding: 20,
-        color: 'white',
-        minheight: 400,
-        width: 250,
-      }}
+
     >
       <div>{column.id}</div>  
       <Droppable droppableId={column.id}>
         {(provided) => (
-          <div ref={provided.innerRef}>
+          <div ref={provided.innerRef}       style={{
+            backgroundColor: 'gray',
+            margin: 10,
+            padding: 20,
+            color: 'white',
+            minHeight: 400,
+            width: 250,
+          }}>
             <div>
               {column.list.map((itemObject, index) => {
                 return <ListItemCustom index={index} itemObject={itemObject} />
